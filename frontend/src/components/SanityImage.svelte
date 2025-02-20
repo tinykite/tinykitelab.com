e313f04ffa232cf9d414addd4d32a337cf6d1720
tinykite <dakota@tinykitelab.com>
2025-02-20 16:17:10 -0600

    Fix image goof

--- frontend/src/components/SanityImage.svelte
+++ frontend/src/components/SanityImage.svelte
@@ -1,49 +1,28 @@
 <script>
 	import { urlFor } from '../lib/utils/image';
-	import { onMount } from 'svelte';
-	import { browser } from '$app/environment';
 
 	const { image } = $props();
 
 	// Example image document ID: image-cc93b69600f5cd1abce97fd0d4aa71793dbbba76-1350x900-png
 	// Structure: image-${storedImgId}-${dimensions}-${format}
-	let maxWidth = 1440;
+	const maxWidth = 1440;
 
 	// If we split it by "-", the 3rd element are the dimensions (1350x900)
-	let dimensions = $derived(image?.asset?._ref?.split('-')[2]);
-	// If we split dimensions by "x", we get the width (1350) and height (900)
-	let [width, height] = $derived(dimensions?.split('x').map(Number) || [null, null]);
-	let [renderedWidth] = $derived(width < maxWidth ? width : maxWidth);
-
-	let imageRef;
-	// Once loaded, the image will transition to full opacity
-	let loaded = $state(false);
-
-	onMount(() => {
-		if (imageRef) {
-			imageRef.onload = () => {
-				loaded = true;
-			};
-		}
-	});
+	const dimensions = $derived(image?.asset?._ref?.split('-')[2]);
+	// const aspectRatio = $derived(
+	// 	parseInt(dimensions?.split('x')[1]) / parseInt(dimensions?.split('x')[0])
+	// );
+	const width = $derived(dimensions?.split('x')[0]);
+	const renderedWidth = $derived(parseInt(width) < maxWidth ? parseInt(width) : maxWidth);
+	// const renderedHeight = $derived(renderedWidth ? Math.ceil(renderedWidth * aspectRatio) : null);
 </script>
 
-{#if browser && image}
-	<img
-		loading="lazy"
-		src={urlFor(image).width(renderedWidth).quality(100).url()}
-		alt={image.alt}
-		class:loaded
-		bind:this={imageRef}
-	/>
+{#if image}
+	<img loading="lazy" src={urlFor(image).width(renderedWidth).quality(100).url()} alt={image.alt} />
 {/if}
 
 <style>
 	img {
-		opacity: 0;
 		transition: opacity 500ms ease-out;
-	}
-	img.loaded {
-		opacity: 1;
 	}
 </style>
